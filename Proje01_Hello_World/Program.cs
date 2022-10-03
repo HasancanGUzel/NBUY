428193b00a399af7a4f359d676813be729e03108
HasancanGUzel <hasancanguzel35@gmail.com>
2022-10-03 18:13:11 +0300

    ilk uygulama

---
+++ Proje01_Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proje01_Hello_World
+{
+    internal class Program// bu programın ana scopunun başlangıç noktası
+    {
+        static void Main(string[] args)// burasıda çalışacak olan metot
+        {
+            Console.WriteLine("Hello, World!");
+           
+        }
+    }
+}
