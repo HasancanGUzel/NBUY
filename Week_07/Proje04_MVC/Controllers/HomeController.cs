af42f0cb86010d8cbf1b26c1ec1190cf8acc6aa5
HasancanGUzel <hasancanguzel35@gmail.com>
2022-11-15 18:09:05 +0300

    15.11.2022

---
+++ Week_07/Proje04_MVC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Proje04_MVC.Controllers;
+
+public class HomeController : Controller
+{
+   
+    public IActionResult Index()
+    {
+        string ad="Hasan";
+        ViewBag.KisiAd=ad;
+        string dil="tr";
+        string selamlama;
+        if (dil=="tr")
+        {
+            selamlama="Hoşgeldin";
+        }
+        else if(dil=="en")
+        {
+            selamlama="Welcome";
+        }
+        else
+        {
+            selamlama="";
+        }
+        ViewBag.Selam=selamlama;
+        return View();
+    }
+
+
+   
+}
