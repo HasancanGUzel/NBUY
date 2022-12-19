4180c2350efde0792693be3bd8ae6acf21a4ad9a
HasancanGUzel <hasancanguzel35@gmail.com>
2022-12-19 18:02:09 +0300

    19.12.2022

---
+++ Week_12/19-12-2022-Roles/ShoppingApp/ShoppingApp.Data/Concrete/EfCore/Contexts/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Entity.Concrete.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Concrete.EfCore.Contexts
+{
+    public class IdentityContext : IdentityDbContext<User,Role,string>//hem user hemde role yani 2 tane entity tanımlarsak yanına 1 tane tip yazmamız lazım
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+    }
+}
