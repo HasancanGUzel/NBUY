4f5ae75c8de94138379f8d1d8c7011042ce2f8dd
HasancanGUzel <hasancanguzel35@gmail.com>
2022-12-27 18:06:14 +0300

    27.12.2022

---
+++ Week_13/27-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/ProductListWithSearchDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class ProductListWithSearchDto // productListDto ve SearchWueryDto yu birleştirdik çünkü bizim index viewımızda önceden ProductListDto tipinde gönderiyorduk ama ona ek olarak SearchQueryDto da gelmişti.
+    {
+        public List<ProductListDto> ProductListDtos { get; set; }
+        public SearchQueryDto SearchQueryDto { get; set; }
+        public List<SelectListItem>IsHomeList{ get; set; }
+        public List<SelectListItem>IsApprovedList{ get; set; }
+    }
+}
---
+++ Week_13/27-12-2022/ShoppingApp/ShoppingApp.Web/Areas/Admin/Models/Dtos/SearchQueryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingApp.Web.Areas.Admin.Models.Dtos
+{
+    public class SearchQueryDto
+    {
+        public bool? IsHome { get; set; } // soru işaretsi koymmızın nedeni boş olabilir
+        public bool? IsApproved { get; set; }
+        public string SearchString { get; set; }
+    }
+}
