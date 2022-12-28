80cb08a1fb0d677ed3a219447151ee2b1ed2ade5
HasancanGUzel <hasancanguzel35@gmail.com>
2022-12-28 15:07:17 +0300

    28.12.2022

---
+++ Week_13/28-12-2022/ShoppingAppClient/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShoppingAppClient.Models;
+using System.Diagnostics;
+
+namespace ShoppingAppClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public async Task<IActionResult> Index()
+        {
+            var urunler = new List<ProductViewModel>();
+            using (var httpClient=new HttpClient())
+            {
+                using (var response= await httpClient.GetAsync("http://localhost:5200/api/products"))
+                {
+                    string apiResponse= await response.Content.ReadAsStringAsync();
+                    urunler=JsonConvert.DeserializeObject<List<ProductViewModel>>(apiResponse);
+                }
+            }
+            return View(urunler);
+        }
+
+      
+    }
+}
